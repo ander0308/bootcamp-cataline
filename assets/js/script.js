a236e396d50c779d5177b34fb85714a52a8772ae
Anderson Santana <ander0308@yahoo.com.br>
2021-03-17 23:10:33 -0300

    upload do projeto

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+const zipCodeField = document.querySelector("#container .box-container input");
+const submitButton = document.querySelector("#container .box-container button");
+const content = document.querySelector(
+  "#container .box-container .zipcode-res"
+);
+
+submitButton.addEventListener("click", run);
+
+function run(event) {
+  event.preventDefault();
+
+  var zipCode = zipCodeField.value;
+
+  zipCode = zipCode.replace(" ", "");
+  zipCode = zipCode.replace(".", "");
+  zipCode = zipCode.trim();
+
+  axios
+    .get("https://viacep.com.br/ws/" + zipCode + "/json/")
+    .then(function (response) {
+      content.innerHTML = "";
+      if (response.data.erro) {
+        throw new Error("CEP invalido");
+      }
+
+      createLine(response.data.logradouro);
+      createLine(response.data.localidade + "/" + response.data.uf);
+      createLine(response.data.bairro);
+      //   console.log(response.data);
+    })
+    .catch(function (error) {
+      content.innerHTML = "";
+      //   console.log(error);
+      createLine("Ops Algo deu errado!");
+    });
+}
+
+function createLine(text) {
+  var line = document.createElement("p");
+  var text = document.createTextNode(text);
+
+  line.appendChild(text);
+  content.appendChild(line);
+}
